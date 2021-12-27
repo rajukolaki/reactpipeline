338cd88d3e331c7969a592d790769e761450acdd
Raj <raju.ko@netwinindia.biz>
2021-12-27 14:18:13 +0530

    second commit for pipeline

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Demo App for Pipeline
-    </div>
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        This Is test from Raju
+      </header>
   );
 }
 
